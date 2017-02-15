52ab935954659c804e850b8b9c90c4a3f2182b63
Yi Zhu <zhu-y11@foxmail.com>
2017-02-15 04:15:06 -0800

    about me + contact

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
 $(document).ready(function() {
     $(".page").each(function(index) {
-	      $(this)
-            .html($("<h3>")
-                  .addClass("page-title")
-		              .text($(this).text()));
+	      $(this).html($("<h3>").addClass("page-title").text($(this).text()));
         $(this).append($("<hr>"));
-	      $(this)
-            .append($("<div>")
-                    .addClass("page-body")
- 	                  .load($(this).attr("id") + ".html"));
+	      $(this).append($("<div>").addClass("page-body").load($(this).attr("id") + ".html"));
+      console.log($(this).attr("id"));
     });
 });
